ce32c54f78966f6bd3a4a3f48d1adb09dc438113
rakumarr <rakumarr@users.noreply.github.com>
2017-04-02 20:11:16 +0530

    New Role Added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO ROLE_INFO (ROLE_NAME, ROLE_DESCRIPTION) VALUES ('Developer', 'Beginner level programmar');
+INSERT INTO ROLE_INFO (ROLE_NAME, ROLE_DESCRIPTION) VALUES ('Tester', 'Tester Joiner Level');
 INSERT INTO EMPLOYEE (FIRSTNAME, LASTNAME, GENDER, ROLE_NAME) VALUES ('rajesh', 'kumar', 'male', 'Developer');
